5e7416fced80792375acba39ba6eacf5844e7ed4
Vasiliy Kevroletin <kevroletin@gmail.com>
2026-02-09 21:03:55 +1000

    play with selection

--- src/main.ts
+++ src/main.ts
@@ -7,67 +7,28 @@ export default class MyPlugin extends Plugin {
   settings: MyPluginSettings;
 
   async onload() {
+    console.log("*** ONLOAD ***");
+
     await this.loadSettings();
 
-    // This creates an icon in the left ribbon.
-    this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-      // Called when the user clicks the icon.
-      new Notice('This is a notice!');
-    });
-
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    const statusBarItemEl = this.addStatusBarItem();
-    statusBarItemEl.setText('Status bar text');
-
-    // This adds a simple command that can be triggered anywhere
-    this.addCommand({
-      id: 'open-modal-simple',
-      name: 'Open modal (simple)',
-      callback: () => {
-        new SampleModal(this.app).open();
-      }
-    });
     // This adds an editor command that can perform some operation on the current editor instance
     this.addCommand({
       id: 'replace-selected',
       name: 'Replace selected content',
       editorCallback: (editor: Editor, view: MarkdownView) => {
-        editor.replaceSelection('Sample editor command');
-      }
-    });
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: 'open-modal-complex',
-      name: 'Open modal (complex)',
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking) {
-            new SampleModal(this.app).open();
-          }
-
-          // This command will only show up in Command Palette when the check function returns true
-          return true;
+        const sel = editor.getSelection().trim();
+        if (sel === "" || sel.contains("\n")) {
+          console.log("Bad file name");
+          return;
         }
-        return false;
+
+        const dir = view.file?.parent?.path ?? "/";
+        const new_file = (dir == "/" ? `${sel}.md` : `${dir}/${sel}.md`);
+
+
+        console.log(`*** ${new_file} ***`)
       }
     });
-
-    // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-      new Notice("Click");
-    });
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
   }
 
   onunload() {
@@ -82,18 +43,3 @@ export default class MyPlugin extends Plugin {
   }
 }
 
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
-
-  onOpen() {
-    let { contentEl } = this;
-    contentEl.setText('Woah!');
-  }
-
-  onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
